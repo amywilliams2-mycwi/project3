8f4684e1350b8313374a20cc56106e9f02861918
Amy <amywilliams2@mycwi.cc>
2015-03-05 16:40:02 -0700

    My initial Project 3 commit

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\Sandbox\webd265\ch04\platforms\android\ant-build\AndroidManifest.xml : \
+c:\Sandbox\webd265\ch04\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+c:\Sandbox\webd265\ch04\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+c:\Sandbox\webd265\ch04\platforms\android\ant-build\CordovaApp.ap_ \
+c:\Sandbox\webd265\ch04\platforms\android\ant-build\classes.dex \
+C:\Sandbox\webd265\ch04\platforms\android\CordovaLib\ant-build\classes.jar \
